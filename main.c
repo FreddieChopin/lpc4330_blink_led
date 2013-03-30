582eec5ef469c28af2af017bc84740600ccd8b38
Freddie Chopin <freddie.chopin@gmail.com>
2013-03-30 11:13:27 +0100

    add empty main()

---
+++ main.c
@@ -0,0 +1,31 @@
+
+/*
+ * Copyright Freddie Chopin 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+/**
+ * \file main.c
+ * \brief Main application.
+ *
+ * chip: LPC4330
+ *
+ * \author Freddie Chopin, http://www.freddiechopin.pl http://www.distortec.com
+ * \date 2013-03-30
+ */
+
+/*---------------------------------------------------------------------------------------------------------------------+
+| global functions
++---------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * \brief Main function.
+ */
+
+int main(void)
+{
+	while (1);
+
+	return 0;
+}
